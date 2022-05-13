52b86d3a9050d02d99888db40af22a9a868d71d9
driss292 <drisskaci@outlook.fr>
2022-05-13 09:27:30 +0200

    ok

--- src/containers/Work.js
+++ src/containers/Work.js
@@ -19,7 +19,7 @@ export default function Work() {
         className="max-w-[1000px] mx-auto p-4 pt-32 pb-32
        flex flex-col justify-center w-full h-full"
       >
-        <div className="pb-8 ">
+        <div className="p-8 ">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Mes projets
           </p>
